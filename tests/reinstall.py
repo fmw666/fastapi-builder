d1a8e543d0a9543766d463f428ae621109fa8d4e
fmw666 <fmw19990718@qq.com>
2023-02-17 20:34:43 +0800

    tests 下代码支持多平台调用

--- tests/reinstall.py
+++ tests/reinstall.py
@@ -1,4 +1,5 @@
 # 仅在测试包时使用
+# 在 fastapi-builder 根目录（即该文件上一级目录）运行
 
 import os
 
@@ -6,19 +7,23 @@ import os
 if __name__ == "__main__":
     # 卸载
     os.system("pip uninstall fastapi-builder")
+
+    # 打包文件所在路径
+    dist_path = "dist/"
     
     # 删除 dist 目录下打包文件
     try:
-        for fname in os.listdir("./dist"):
+        for fname in os.listdir(dist_path):
             if fname.endswith(".whl"):
-                os.remove(".\dist\\" + fname)
+                os.remove(os.path.join(dist_path, fname))
                 break
     except:
         pass
 
     # 打包并安装
-    os.system("python .\setup.py bdist_wheel")
-    for fname in os.listdir("./dist"):
+    os.system("python ./setup.py bdist_wheel")
+    for fname in os.listdir(dist_path):
         if fname.endswith(".whl"):
-            os.system("pip install .\dist\\" + fname)
+            file_path = os.path.join(dist_path, fname)
+            os.system(f"pip install {file_path}")
             break
